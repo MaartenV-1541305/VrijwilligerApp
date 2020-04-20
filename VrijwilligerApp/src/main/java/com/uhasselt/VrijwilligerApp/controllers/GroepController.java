bc00a6d3f3f60379162a5cbd10a9454cdd23b4d2
git-manager <vandenboer@Arch-Linux.vandenhoudt.local>
2020-04-20 02:13:47 +0200

    @RestController vergeten bij groepController

--- VrijwilligerApp/src/main/java/com/uhasselt/VrijwilligerApp/controllers/GroepController.java
+++ VrijwilligerApp/src/main/java/com/uhasselt/VrijwilligerApp/controllers/GroepController.java
@@ -16,7 +16,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 /**
  *
  * @author vandenboer
@@ -78,6 +80,5 @@ public class GroepController {
     public void groepAanmakenBeeindigen(int groepId) {
         //TODO: stuur afrondings bericht
     }
-    
     
 }
